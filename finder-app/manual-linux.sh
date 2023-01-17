b5bd61e74992b3d89748a05b9198fbe5968392a8
Predtech <predtech4@yandex.ru>
2023-01-17 07:08:19 +0300

    busybox project

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
 
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 
 if [ $# -lt 1 ]
 then
@@ -24,35 +25,51 @@ fi
 mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
+
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    # make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- ...
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig CONFIG_PVPANIC=y
+    # PATCH THERE
+    git apply ${FINDER_APP_DIR}/dtc-lexer.patch
+
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp -a ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    sudo rm -rf rootfs
 fi
 
-# TODO: Create necessary base directories
+mkdir rootfs
+cd rootfs
+mkdir bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir usr/bin usr/lib usr/sbin 
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
@@ -61,20 +78,55 @@ else
 fi
 
 # TODO: Make and install busybox
+sudo make distclean
+sudo make defconfig
+
+# CROSS_COMPILE can't be found when using sudo. Have to write absolute path.
+make ARCH=$ARCH LDFLAGS="--static" CROSS_COMPILE=$CROSS_COMPILE install CONFIG_PREFIX="${OUTDIR}/rootfs"
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+cd "${OUTDIR}/rootfs"
+#${CROSS_COMPILE}readelf -a bin/busybox | grep NEEDED
 
 # TODO: Add library dependencies to rootfs
 
+cp -a "${SYSROOT}/lib/ld-linux-aarch64.so.1" lib
+cp -a "${SYSROOT}/lib64/ld-2.31.so" lib64
+cp -a "${SYSROOT}/lib64/libc-2.31.so" lib64
+cp -a "${SYSROOT}/lib64/libm.so.6" lib64
+cp -a "${SYSROOT}/lib64/libm-2.31.so" lib64
+cp -a "${SYSROOT}/lib64/libresolv.so.2" lib64
+cp -a "${SYSROOT}/lib64/libresolv-2.31.so" lib64
+cp -a "${SYSROOT}/lib64/libc.so.6" lib64
+
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+make -C $FINDER_APP_DIR CROSS_COMPILE=$CROSS_COMPILE clean
+make -C $FINDER_APP_DIR CROSS_COMPILE=$CROSS_COMPILE all
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp -a $FINDER_APP_DIR/writer home
+cp -a $FINDER_APP_DIR/finder.sh home
+cp -a $FINDER_APP_DIR/finder-test.sh home
+cp -a $FINDER_APP_DIR/autorun-qemu.sh home
+
+mkdir home/conf
+cp -a $FINDER_APP_DIR/conf/username.txt home/conf 
+
+touch init
+
+echo "#!/bin/sh\necho \"im in init\"\nmount -t devtmpfs  devtmpfs  /dev\nmount -t proc      proc      /proc\nmount -t sysfs     sysfs     /sys\nmount -t tmpfs     tmpfs     /tmp\nexec /bin/sh" > init
+chmod +x init
 
 # TODO: Chown the root directory
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+find . -print0 | cpio --null -ov --format=newc > ${OUTDIR}/initramfs.cpio
+gzip ${OUTDIR}/initramfs.cpio
+
+
